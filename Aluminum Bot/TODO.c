419c63de0d30c7f086abde49534f4a38b027bbaa
Kevin Selavko <kselavko@gmail.com>
2014-03-01 15:14:46 -0500

    Before I screw everything up

--- Aluminum Bot/TODO.c
+++ Aluminum Bot/TODO.c
@@ -3,7 +3,7 @@
 |
 | * Figure out why encoders are reseting when sent a value
 | * Get armPID to not kill the breakers
-|    - If I get it working, rimplement it
+|    - If I get it working, rimplement it in compitition code
 | * Get more than 5 options for auton selection
 |    - 2 original blue
 |    - 2 original red
@@ -12,13 +12,9 @@
 |    - Dump
 |    - 2 that dont cross over the bump and potentially wreck other robots
 |       ~ 9 total positions
+| * TEST 2.0 Keys
 |
 | * Finish TODO
-|
-|
-|
-|
-|
 |
 |
 \*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
